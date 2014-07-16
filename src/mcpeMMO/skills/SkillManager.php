a6fc118d7c445a314c165d47b633cc319d9ac971
Samuel <Samuel_ipad2@hotmail.com>
2014-07-16 15:14:55 +1000

    Update SkillManager.php

--- src/mcpeMMO/skills/SkillManager.php
+++ src/mcpeMMO/skills/SkillManager.php
@@ -1,6 +1,34 @@
 <?php
 namespace mcpeMMO/skills;
 
+use mcpeMMO\skills\acrobatics\Acrobatics;
+use mcpeMMO\skills\acrobatics\AcrobaticsManager;
+use mcpeMMO\skills\archery\Archery;
+use mcpeMMO\skills\archery\ArcheryManager;
+use mcpeMMO\skills\archery\TrackedEntity;
+use mcpeMMO\skills\axes\Axes;
+use mcpeMMO\skills\axes\AxesManager;
+use mcpeMMO\skills\child\ChildConfig;
+use mcpeMMO\skills\child\FamilyTree;
+use mcpeMMO\skills\excavation\Excavation;
+use mcpeMMO\skills\excavation\ExcavationManager;
+use mcpeMMO\skills\herbalism\Herbalism;
+use mcpeMMO\skills\herbalism\HerbalismManager;
+use mcpeMMO\skills\repair\Repair;
+use mcpeMMO\skills\repair\RepairManager;
+use mcpeMMO\skills\salvage\Salvage;
+use mcpeMMO\skills\salvage\SalvageManager;
+use mcpeMMO\skills\smelting\Smelting;
+use mcpeMMO\skills\smelting\SmeltingManager;
+use mcpeMMO\skills\swords\Swords;
+use mcpeMMO\skills\swords\SwordsManager;
+use mcpeMMO\skills\taming\Taming;
+use mcpeMMO\skills\taming\TamingManager;
+use mcpeMMO\skills\unarmed\Unarmed;
+use mcpeMMO\skills\unarmed\UnarmedManager;
+use mcpeMMO\skills\woodcutting\Woodcutting;
+use mcpeMMO\skills\woodcutting\WoodcuttingManager;
+
 public class SkillManager(
   //this will be added to later
 }
